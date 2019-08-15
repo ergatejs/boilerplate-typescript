e65febb205599eded8bb29fea6222eca94d1500c
Suyi <thonatos.yang@gmail.com>
2019-08-15 10:53:22 +0800

    feat: enable codecov

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  "roots": [
-    "./test"
-  ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
+  roots: ['./test'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
   },
-}
+  "collectCoverage": true
+};
